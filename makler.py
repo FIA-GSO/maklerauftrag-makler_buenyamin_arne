ed981e6dc2cd411cd42b1c87f6afd572a71c6606
Gueclue <BGUECLUE@ford.com>
2024-03-10 13:00:14 +0100

    final

--- makler.py
+++ makler.py
@@ -1,57 +1,61 @@
-Dictionary = {} #dictionary in welhem die Räumen + Größen gespeichert werden
+dictionary = {}  # Noch leeres Wörterbuch um Größen und Raumnamen zu speichern.
 
-AnzahlDerRaeume = input("Wie viele Räume gibt es? ") 
+anzahl_der_raeume = input("Wie viele Räume gibt es? (Bitte eine ganze Zahl eingeben!) ")
+anzahl_der_raeume = int(anzahl_der_raeume)  # Anzahl der Räume in Integer konvertieren
+raum_art = str  # Eine Variable die später benötigt wird als String
 
-AnzahlDerRaeume = int(AnzahlDerRaeume) # anzahl der räume in ein integer konvertieren
+for Raum in range(anzahl_der_raeume):
+    raum_breite = 0
+    raum_laenge = 0
+    raum_groeße = 0  # Um Fehler umzugehen, Variablen am anfang des Algorithmus auf 0 setzen
+    raum_name = input("Wie soll der Raum heißen? ")
 
-RaumArt = str # eine variable die später benöigt wird
+    while True:
+        raum_art = input("Hat der Raum genau 4 Ecken? Ja oder Nein ")
+        if raum_art in ["ja", "Ja", "nein", "Nein"]:
+            #Sicher gehen das der Input nur: ja/Ja/nein/Nein sein darf!
+            break
+        else:
+            print("Fehlerhafte Eingabe! Bitte versuchen Sie es nochmal!")
 
-for Raum in range(AnzahlDerRaeume):
-        RaumBreite = 0
-        RaumLaenge = 0
-        RaumGroeße = 0 # um Fehler zu umgehen variablen am anfang des algorithmus auf 0 setzen
-        RaumName = input("Wie soll der Raum heißen? ")
-        while (True):
-            RaumArt = input("Hat der Raum genau 4 Ecken? Ja oder Nein ")
-            if (RaumArt == "ja" or RaumArt == "Ja" or RaumArt == "Nein" or RaumArt == "nein"): # sicher gehen das man nur ein richtigen input eingeben kann
-                break
-        if(RaumArt == "Ja" or RaumArt == "ja"): #falls der raum 4 ecken hat wird hier die abfrage zu diesem gemacht
-            RaumBreite = int(input("RaumBreite in cm: "))
-            RaumLaenge = int(input("RaumLaenge in cm: "))
-            RaumGroeße = RaumLaenge * RaumBreite
-            Dictionary.setdefault(RaumName, RaumGroeße) #die variabelen werden hier gespeichert
-            print("Raum wurde hinzugefügt")
+    if raum_art in ["ja", "Ja"]:  # Falls der Raum 4 ecken hat wird hier die abfrage für den Fall gemacht
+        raum_breite = int(input("RaumBreite in cm: "))
+        raum_laenge = int(input("RaumLaenge in cm: "))
+        raum_groeße = raum_laenge * raum_breite
+        dictionary.setdefault(raum_name, raum_groeße)  # Die Variablen werden hier gespeichert
+        print("Raum wurde hinzugefügt")
 
-        if(RaumArt == "Nein" or RaumArt == "nein"):
-            RaumArt = input("Kann der Raum in mehrere Vierecke geteilt werden? Ja oder Nein ") # falls der raum mehr als 4 ecken hat werden hier die weitern abfragen getätigt
-            if(RaumArt == "Ja" or RaumArt == "ja"):
-                while True:
-                    RaumBreite = RaumBreite + int(input("RaumBreite in cm: "))
-                    RaumLaenge = RaumLaenge + int(input("RaumLaenge in cm: "))
-                    NextInput = input ("eine Weitere Länge / Breite? Ja oder Nein ")# falls man weitere längen / breiten eingeben will wird dies hier abgefragt
-                    if (NextInput == "Nein" or NextInput == "nein" ):
-                        RaumGroeße = RaumLaenge * RaumBreite
-                        Dictionary.setdefault(RaumName, RaumGroeße)# hier wird der raum gespeichert
-                        print("Raum wurde hinzugefügt")
-                        break
-                    
-            if(RaumArt == "Nein" or RaumArt == "nein"):
-                NextInput("wollen Sie die Raum Größe selbstständig berechnen? Ja oder Nein")
-                if (NextInput == "Nein" or NextInput == "nein" ):# falls man den raum nicht eingeben will wird hier gemacht
+    # Falls der Raum mehr als 4 ecken hat werden hier die weitern Abfragen getätigt
+    if raum_art == "Nein" or raum_art == "nein":
+        raum_art = input("Kann der Raum in mehrere Vierecke geteilt werden? Ja oder Nein ")
+        if raum_art == "Ja" or raum_art == "ja":
+            while True:
+                raum_breite = raum_breite + int(input("RaumBreite in cm: "))
+                raum_laenge = raum_laenge + int(input("RaumLaenge in cm: "))
+                # Falls man weitere längen / breiten eingeben will wird dies hier abgefragt
+                next_input = input("Eine weitere Länge / Breite hinzufügen? Ja oder Nein ")
+                if next_input == "Nein" or next_input == "nein":
+                    raum_groeße = raum_laenge * raum_breite
+                    dictionary.setdefault(raum_name, raum_groeße)  # Hier werden die Rauminformationen gespeichert
+                    print("Raum wurde hinzugefügt")
                     break
-                else:
-                    RaumGroeße = input ("geben sie die Größe ihres Raums ein.")
-                    Dictionary.setdefault(RaumName, RaumGroeße)# der eigene raum wird hier bespeichert
-                    print("Raum wurde hinzugefügt") 
-        
 
+        if raum_art == "Nein" or raum_art == "nein":
+            next_input = input("Wollen Sie die Raum Größe selbstständig berechnen? Ja oder Nein")
+            if next_input == "Nein" or next_input == "nein":  # Falls: Raum nicht eingegeben wird : Schleife beenden
+                break
+            else:
+                raum_groeße = input("Geben sie die Größe ihres Raums ein.")
+                dictionary.setdefault(raum_name, raum_groeße)  # Der Eigene Raum wird hier gespeichert
+                print("Raum wurde hinzugefügt")
 
-for wert in Dictionary:
-    print (f"{wert} : {Dictionary.get(wert)}cm²")# die inhalte des dictionary werden hier ausgegeben
+# Ausgabe der Namen und Größen der Räume im Dictionary
+for wert in dictionary:
+    print(f"{wert} : {dictionary.get(wert)}cm²")  # Die Inhalte des Dictionary werden hier ausgegeben
 
-Wohnungsfläche = 0 
+wohnungsfläche = 0
+# Berechnung und Ausgabe der kompletten Wohnfläche
+for wert in dictionary:
+    wohnungsfläche = wohnungsfläche + dictionary.get(wert)
 
-for wert in Dictionary:
-    Wohnungsfläche = Wohnungsfläche + Dictionary.get(wert)
-
-print (f"insgesamte Wohnungsfläche {Wohnungsfläche}cm²")# die insgesamt wohnfläche wird hier ausgegeben
+print(f"insgesamte Wohnungsfläche {wohnungsfläche}cm²")  # Die gesamte Wohnfläche wird hier ausgegeben
